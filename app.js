d575aca10f66222a0a200b9e6ca882a96e7468a0
lomserg <lomserg@gmail.com>
2023-11-15 10:13:07 +0300

    feat: add save function, cleanup code

--- app.js
+++ app.js
@@ -25,6 +25,19 @@ Player.prototype.addScore = function () {
   this.score++;
   // Update the HTML to reflect the new score
   this.player.querySelector(".score").innerHTML = this.score;
+  // Save to local storage
+
+  let scorePlayerObj = JSON.stringify({
+    name: `${this.name}`,
+    score: `${this.score}`,
+  });
+
+  console.log(scorePlayerObj);
+  window.localStorage.setItem(
+    `${this.name}Score`,
+    JSON.stringify(scorePlayerObj)
+  );
+  this.init();
 };
 
 Player.prototype.removeScore = function () {
@@ -32,6 +45,17 @@ Player.prototype.removeScore = function () {
   this.score--;
   // Update the HTML to reflect the new score
   this.player.querySelector(".score").innerHTML = this.score;
+  let scorePlayerObj = JSON.stringify({
+    name: `${this.name}`,
+    score: `${this.score}`,
+  });
+
+  console.log(scorePlayerObj);
+  window.localStorage.setItem(
+    `${this.name}Score`,
+    JSON.stringify(scorePlayerObj)
+  );
+  this.init();
 };
 
 Player.prototype.init = function () {
@@ -47,88 +71,3 @@ Player.prototype.init = function () {
 
 const serg = new Player("serg", document.querySelector("#player1"));
 const danyil = new Player("danyil", document.querySelector("#player2"));
-
-console.log(serg);
-console.log(danyil);
-// const btns = document.querySelectorAll(".btn");
-// const scorePlayer2 = document.querySelector("#score-player2");
-// const scorePlayer1 = document.querySelector("#score-player1");
-// const raitingDniyl = document.querySelector("#score-player2");
-// const turtSerg = document.querySelector(".turtle-img-serg");
-// const turtDanyil = document.querySelector(".turtle-img-danyil");
-
-// let sergScore;
-// let danyilScore;
-// btn serg
-function Game() {
-  const addScoreSerg = document.querySelector("#add-score-player2");
-  const removeScoreSerg = document.querySelector("#remove-score-player2");
-}
-
-// function init() {
-//   let player2Score = JSON.parse(window.localStorage.getItem("sergScore"));
-//   let player1Score = JSON.parse(window.localStorage.getItem("danyilScore"));
-//   player2Score = JSON.parse(player2Score);
-//   serg.score = player2Score.score;
-//   player1Score = JSON.parse(player1Score);
-//   danyil.score = player1Score.score;
-//   turtSerg.setAttribute("style", ` --x: ${serg.score * 10}px;`);
-//   turtDanyil.setAttribute("style", ` --x: ${serg.score * 10}px;`);
-//   scorePlayer2.textContent = serg.score;
-//   scorePlayer1.textContent = danyil.score;
-// }
-
-// init();
-// function hahdleRaiting(e) {
-//   console.log(e.target.id.includes("add") && e.target.id.includes("player1"));
-//   if (e.target.id.includes("add") && e.target.id.includes("player2")) {
-//     serg.addScore(1);
-//     scorePlayer2.textContent = serg.score;
-//     sergScore = JSON.stringify(serg);
-//     window.localStorage.setItem("sergScore", JSON.stringify(sergScore));
-//     turtSerg.setAttribute("style", ` --x: ${serg.score * 10}px;`);
-//   } else if (e.target.id.includes("add") && e.target.id.includes("player1")) {
-//     danyil.addScore(1);
-//     scorePlayer1.textContent = danyil.score;
-//     danyilScore = JSON.stringify(danyil);
-//     window.localStorage.setItem("danyilScore", JSON.stringify(danyilScore));
-//     turtDanyil.setAttribute("style", ` --x: ${danyil.score * 10}px;`);
-//   } else if (
-//     e.target.id.includes("remove") &&
-//     e.target.id.includes("player1")
-//   ) {
-//     danyil.removeScore(1);
-//     scorePlayer1.textContent = danyil.score;
-//     danyilScore = JSON.stringify(danyil);
-//     window.localStorage.setItem("danyilScore", JSON.stringify(danyilScore));
-//     turtDanyil.setAttribute("style", ` --x: ${danyil.score * 10}px;`);
-//   } else if (
-//     e.target.id.includes("remove") &&
-//     e.target.id.includes("player2")
-//   ) {
-//     serg.removeScore(1);
-//     scorePlayer2.textContent = serg.score;
-//     sergScore = JSON.stringify(serg);
-//     window.localStorage.setItem("sergScore", JSON.stringify(sergScore));
-//     turtSerg.setAttribute("style", ` --x: ${serg.score * 10}px;`);
-//   }
-// }
-
-// addScoreSerg.addEventListener("click", addRaiting);
-
-// removeScoreSerg.addEventListener("click", () => {
-//   serg.removeScore(1);
-//   scorePlayer2.textContent = serg.score;
-//   sergScore = JSON.stringify(serg);
-//   console.log(sergScore);
-//   window.localStorage.setItem("sergScore", JSON.stringify(sergScore));
-//   turtSerg.setAttribute("style", ` --x: ${serg.score * 10}px;`);
-// });
-
-// console.log(serg.score);
-
-// console.log(JSON.parse(sergScore));
-
-// btns.forEach((btn) => {
-//   btn.addEventListener("click", hahdleRaiting);
-// });
